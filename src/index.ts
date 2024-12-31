5152c6fb241b59e524a3f4d14b885863da0d80fd
Moritz Utcke <letsmoe1404@gmail.com>
2024-12-31 09:07:36 +0700

    Fix

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { createBilinearInterpolationFunction } from './bilinear';
 export { linearInterpolation } from "./linear"
 export { quadraticInterpolation } from "./quadratic"
 export { lagrangeInterpolation } from "./lagrange"
+export { nevillePolynomialInterpolation } from "./neville"
 export type { Vector2D, Vector3D } from "./shape"
